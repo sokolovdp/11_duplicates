81a06621b60736dca506210534072fedb593d9a1
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-07-04 22:24:37 +0300

    Code modified according to MVC pattern

--- duplicates.py
+++ duplicates.py
@@ -3,39 +3,45 @@ import os
 from collections import Counter
 
 
-def print_duplicates(size: "int", files: "list"):
-    print("-" * 40)
-    for file in files:
-        if file[1] == size:
-            print(file[2])
+def print_duplicates(all_duplicate_files: "list of lists"):
+    for duplicate_files in all_duplicate_files:
+        print("-" * 40)
+        for file in duplicate_files:
+            print("name='{}' size={} path='{}'".format(*file))
 
 
-def find_duplicates(files: "list of dicts"):
-    duplicate_names = [name for name, val in Counter([file['name'] for file in files]).items() if val > 1]
+def find_duplicates(all_files: "list of dicts") -> "list":
+    duplicate_names = [name for name, val in Counter([file['name'] for file in all_files]).items() if val > 1]
 
-    duplicate_full = list()
+    potential_duplicates = list()
     for name in duplicate_names:
-        list_with_sizes = list()
-        for file in files:
-            if file['name'] == name:
-                list_with_sizes.append((file['name'], file['size'], file['path']))
-        duplicate_full.append(list_with_sizes)
+        names_with_sizes = [(file['name'], file['size'], file['path']) for file in all_files if file['name'] == name]
+        potential_duplicates.append(names_with_sizes)
 
-    for files_with_one_name in duplicate_full:
-        sizes = Counter([file[1] for file in files_with_one_name])
+    full_duplicates = list()
+    for files_with_one_name in potential_duplicates:
+        sizes = [size for size, val in Counter([file[1] for file in files_with_one_name]).items() if val > 1]
+
         for size in sizes:
-            print_duplicates(size, files_with_one_name)
+            one_name_one_size_files = [file for file in files_with_one_name if file[1] == size]
+            full_duplicates.append(one_name_one_size_files)
+
+    return full_duplicates
 
 
-def main(pathname: "str"):
+def get_all_files_info(pathname: "str") -> "list of tuples":
     all_files = list()
-    for path, subdirs, files in os.walk(pathname):
+    for path, sub_dirs, files in os.walk(pathname):
         for filename in files:
             full_path = os.path.join(path, filename)
             all_files.append({'name': filename, 'size': os.path.getsize(full_path), 'path': full_path})
+    return all_files
 
-    find_duplicates(all_files)
 
+def main(pathname: "str"):
+    all_files_info = get_all_files_info(pathname)
+    all_duplicate_files = find_duplicates(all_files_info)
+    print_duplicates(all_duplicate_files)
 
 if __name__ == '__main__':
     main(sys.argv[1:][0])
