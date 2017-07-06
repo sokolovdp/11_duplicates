9278cfacbf01f108b77835d3c22e815baf2ec078
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-07-06 03:04:46 +0300

    var name file changed to f for iteration over lists deleted comments, plus var names adjusted sys.argv[1:][0] changed to sys.argc[1] assert statement changed to check path validity simplified logic: use file descriptor vs tuple

--- duplicates.py
+++ duplicates.py
@@ -3,32 +3,30 @@ import os
 from collections import Counter
 
 
-def print_duplicates(all_duplicate_files: "list of lists"):
+def print_duplicates(all_duplicate_files: "list"):
     for duplicate_files in all_duplicate_files:
         print()
-        for file in duplicate_files:
-            print("file='{}' ({} bytes)".format(file[2], file[1]))
+        for file_data in duplicate_files:
+            print("file='{}' ({} bytes)".format(file_data['path'], file_data['size']))
 
 
-def find_duplicates(all_files: "list of dicts") -> "list of lists":
-    # find files with duplicate names
-    duplicate_names = [name for name, val in Counter([file['name'] for file in all_files]).items() if val > 1]
+def find_duplicates(all_files: "list") -> "list":
+    duplicate_names = [name for name, val in Counter([f['name'] for f in all_files]).items() if val > 1]
     potential_duplicates = list()
     for name in duplicate_names:
-        names_with_sizes = [(file['name'], file['size'], file['path']) for file in all_files if file['name'] == name]
-        potential_duplicates.append(names_with_sizes)
-    # find files with same name and same size
-    full_duplicates = list()
+        potential_duplicates.append([f for f in all_files if f['name'] == name])
+
+    duplicate_names_and_sizes = list()
     for files_with_one_name in potential_duplicates:
-        duplicate_sizes = [size for size, val in Counter([file[1] for file in files_with_one_name]).items() if val > 1]
+        duplicate_sizes = [size for size, val in Counter([f['size'] for f in files_with_one_name]).items() if val > 1]
         for size in duplicate_sizes:
-            one_name_one_size_files = [file for file in files_with_one_name if file[1] == size]
-            full_duplicates.append(one_name_one_size_files)
+            one_name_one_size_files = [f for f in files_with_one_name if f['size'] == size]
+            duplicate_names_and_sizes.append(one_name_one_size_files)
 
-    return full_duplicates
+    return duplicate_names_and_sizes
 
 
-def get_all_files_info(pathname: "str") -> "list of tuples":
+def get_all_files_info(pathname: "str") -> "list":
     all_files = list()
     for path, sub_dirs, files in os.walk(pathname):
         for filename in files:
@@ -42,8 +40,10 @@ def main(path_name: "str"):
     all_duplicate_files = find_duplicates(all_files_info)
     print_duplicates(all_duplicate_files)
 
-if __name__ == '__main__':
-    dir_name = sys.argv[1:][0]
-    assert os.path.exists(dir_name)
 
-    main(dir_name)
+if __name__ == '__main__':
+    dir_name = sys.argv[1]
+    if not os.path.exists(dir_name):
+        print("invalid path {}".format(dir_name))
+    else:
+        main(dir_name)
