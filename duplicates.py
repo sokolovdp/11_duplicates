dfa716de8e9c4beea48cd104275351ee3bc2e080
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-07-06 15:45:52 +0300

    improved naming

--- duplicates.py
+++ duplicates.py
@@ -6,27 +6,26 @@ from collections import Counter
 def print_duplicates(all_duplicate_files: "list"):
     for duplicate_files in all_duplicate_files:
         print()
-        for file_data in duplicate_files:
-            print("file='{}' ({} bytes)".format(file_data['path'], file_data['size']))
+        for file_info in duplicate_files:
+            print("file='{}' ({} bytes)".format(file_info['path'], file_info['size']))
 
 
 def find_duplicates(all_files: "list") -> "list":
     duplicate_names = [name for name, val in Counter([f['name'] for f in all_files]).items() if val > 1]
     potential_duplicates = list()
     for name in duplicate_names:
-        potential_duplicates.append([f for f in all_files if f['name'] == name])
-
+        potential_duplicates.append([file_info for file_info in all_files if file_info['name'] == name])
     duplicate_names_and_sizes = list()
     for files_with_one_name in potential_duplicates:
-        duplicate_sizes = [size for size, val in Counter([f['size'] for f in files_with_one_name]).items() if val > 1]
+        duplicate_sizes = [size for size, val in
+                           Counter([file_info['size'] for file_info in files_with_one_name]).items() if val > 1]
         for size in duplicate_sizes:
-            one_name_one_size_files = [f for f in files_with_one_name if f['size'] == size]
+            one_name_one_size_files = [file_info for file_info in files_with_one_name if file_info['size'] == size]
             duplicate_names_and_sizes.append(one_name_one_size_files)
-
     return duplicate_names_and_sizes
 
 
-def get_all_files_info(pathname: "str") -> "list":
+def load_all_files_info(pathname: "str") -> "list":
     all_files = list()
     for path, sub_dirs, files in os.walk(pathname):
         for filename in files:
@@ -36,7 +35,7 @@ def get_all_files_info(pathname: "str") -> "list":
 
 
 def main(path_name: "str"):
-    all_files_info = get_all_files_info(path_name)
+    all_files_info = load_all_files_info(path_name)
     all_duplicate_files = find_duplicates(all_files_info)
     print_duplicates(all_duplicate_files)
 
