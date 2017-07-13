e39ffd4787ccc48fd244e2e8ad80a50e1a8a70df
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-07-13 14:03:29 +0300

    used groupby function to simplify find_duplites function

--- duplicates.py
+++ duplicates.py
@@ -1,23 +1,18 @@
 import sys
 import os
-from itertools import groupby
+from collections import Counter
 
 
-def print_duplicates(all_duplicate_files: "list"):
-    for duplicate_files in all_duplicate_files:
+def print_duplicates(all_files_info: "list", duplicated_files: "list"):
+    for file in duplicated_files:
         print()
-        for file_info in duplicate_files:
-            print("file='{}' ({} bytes)".format(file_info['path'], file_info['size']))
+        for info in all_files_info:
+            if info[0] == file:
+                print("file='{}' ({}) bytes".format(info[1], info[0][1]))
 
 
-def find_duplicates(all_files_info: "list") -> "list":
-    def key_fun(file_info):
-        return file_info['name'], file_info['size']
-
-    grouped_info = [[key, list(group)] for key, group in groupby(sorted(all_files_info, key=key_fun), key_fun)]
-    duplicates = [group[1] for group in grouped_info if len(group[1]) > 1]
-
-    return duplicates
+def find_duplicated_files(all_files_info: "list") -> "list":
+    return [file for file, count in dict(Counter(info[0] for info in all_files_info)).items() if count > 1]
 
 
 def load_all_files_info(pathname: "str") -> "list":
@@ -25,14 +20,14 @@ def load_all_files_info(pathname: "str") -> "list":
     for path, sub_dirs, files in os.walk(pathname):
         for filename in files:
             full_path = os.path.join(path, filename)
-            all_files_info.append({'name': filename, 'size': os.path.getsize(full_path), 'path': full_path})
+            all_files_info.append(((filename, os.path.getsize(full_path)), full_path))
     return all_files_info
 
 
 def main(path_name: "str"):
     all_files_info = load_all_files_info(path_name)
-    all_duplicate_files = find_duplicates(all_files_info)
-    print_duplicates(all_duplicate_files)
+    duplicated_files = find_duplicated_files(all_files_info)
+    print_duplicates(all_files_info, duplicated_files)
 
 
 if __name__ == '__main__':
