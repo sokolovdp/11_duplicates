52c76b4816858f0d2611a38f9cde5b0be96644e3
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-07-15 12:41:45 +0300

    introduced namedtuple: File_descriptor and File_key changed functions: find_duplicated_files and print_duplicates updated vars names

--- duplicates.py
+++ duplicates.py
@@ -1,33 +1,43 @@
-import sys
 import os
-from collections import Counter
+import sys
+from collections import Counter, namedtuple
+
+File_key = namedtuple('File_key', ['file_name', 'file_size'])
+File_descriptor = namedtuple('File_descriptor', ['file_key', 'file_path'])
 
 
-def print_duplicates(all_files_info: "list", duplicated_files: "list"):
-    for file in duplicated_files:
+def print_duplicates(all_duplicated_files: "list"):
+    for duplicated_files in all_duplicated_files:
         print()
-        for info in all_files_info:
-            if info[0] == file:
-                print("file='{}' ({}) bytes".format(info[1], info[0][1]))
+        for descriptor in duplicated_files:
+             print("file='{}' ({}) bytes".format(descriptor.file_path, descriptor.file_key.file_size))
 
 
-def find_duplicated_files(all_files_info: "list") -> "list":
-    return [file for file, count in dict(Counter(info[0] for info in all_files_info)).items() if count > 1]
+def find_duplicated_files(all_file_descriptors: "list") -> "list":
+    counted_files_keys = Counter([descriptor.file_key for descriptor in all_file_descriptors])
+    duplicated_files_keys = [file_key for file_key, repeated in counted_files_keys.items() if repeated > 1]
+    all_duplicated_files = list()
+    for file_key in duplicated_files_keys:
+        all_duplicated_files.append(
+            [descriptor for descriptor in all_file_descriptors if descriptor.file_key == file_key])
+    return all_duplicated_files
 
 
-def load_all_files_info(pathname: "str") -> "list":
-    all_files_info = list()
-    for path, sub_dirs, files in os.walk(pathname):
-        for filename in files:
-            full_path = os.path.join(path, filename)
-            all_files_info.append(((filename, os.path.getsize(full_path)), full_path))
-    return all_files_info
+def load_all_file_descriptors(path_name: "str") -> "list":
+    all_file_descriptors = list()
+    for path, sub_dirs, files in os.walk(path_name):
+        for file_name in files:
+            full_path = os.path.join(path, file_name)
+            file_size = os.path.getsize(full_path)
+            file_key = File_key(file_name, file_size)
+            all_file_descriptors.append(File_descriptor(file_key, full_path))
+    return all_file_descriptors
 
 
 def main(path_name: "str"):
-    all_files_info = load_all_files_info(path_name)
-    duplicated_files = find_duplicated_files(all_files_info)
-    print_duplicates(all_files_info, duplicated_files)
+    all_file_descriptors = load_all_file_descriptors(path_name)
+    all_duplicated_files = find_duplicated_files(all_file_descriptors)
+    print_duplicates(all_duplicated_files)
 
 
 if __name__ == '__main__':
